cd3c452df09015541c99e7a24a4c4f2ee58ac69e
FlavioGomesMenezesPorto <flavioporto203@gmail.com>
2025-09-09 17:05:48 -0300

    1

--- src/components/sections/CartaApresentacao.jsx
+++ src/components/sections/CartaApresentacao.jsx
@@ -3,7 +3,7 @@ const CartaApresentacao = () => {
     <section className="section carta-apresentacao-section">
       <h2 className="section-title">Carta de Apresentação</h2>
       <p>
-        Olá! Meu nome é Flávio Gomes Menezes Porto. Sou apaixonado por tecnologia e inovação, com experiência em desenvolvimento de sistemas, automação e inteligência artificial.
+        Olá! Meu nome é Flávio Gomes Menezes Porto. Sou apaixonado por tecnologia e inovação, com experiência em desenvolvimento de sistemas.
         Busco sempre entregar soluções digitais que agreguem valor e eficiência para meus clientes.
         Estou pronto para transformar ideias em projetos reais, com dedicação, ética e profissionalismo.
       </p>
